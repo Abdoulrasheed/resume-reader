0f55d13c0cce8af086fdaabb38e8b4f7c231b1f3
abdoulrasheed <abdoulrasheedibrahim47@gmail.com>
2022-02-07 23:56:50 +0100

    Add pythonanyhwere to allowed hosts

--- cvextract/settings.py
+++ cvextract/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-+hba+tsx$q8jl7wt$m=mfdkm00vhn%c&9j&rlvdpi5ak@phoj1
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', 'cvreader.pythonanywhere.com']
 
 
 # Application definition
